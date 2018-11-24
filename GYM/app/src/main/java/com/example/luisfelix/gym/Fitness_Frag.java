ad66e16705a87322d12fb5e31f68b3bebb5f867b
Proyecto final <lfelix3011@gmail.com>
2018-11-24 10:10:33 -0400

    kare ya

--- GYM/app/src/main/java/com/example/luisfelix/gym/Fitness_Frag.java
+++ GYM/app/src/main/java/com/example/luisfelix/gym/Fitness_Frag.java
@@ -2,10 +2,15 @@ package com.example.luisfelix.gym;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +18,46 @@ import android.view.ViewGroup;
  */
 public class Fitness_Frag extends Fragment {
 
+    FloatingActionButton masAgua, menosAgua;
+    TextView textAgua;
+    int contador=0;
 
     public Fitness_Frag() {
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fitness, container, false);
+        View v=inflater.inflate(R.layout.fragment_fitness, container, false);
+
+        masAgua=v.findViewById(R.id.btnAgua_mas);
+        menosAgua=v.findViewById(R.id.btnAgua_menos);
+        textAgua=v.findViewById(R.id.CantidadAgua);
+
+        masAgua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    contador++;
+                    textAgua.setText(" "+contador);
+            }
+        });
+        menosAgua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador--;
+                textAgua.setText(" "+contador);
+            }
+        });
+
+        return v;
     }
+   /* public void masAgua(View view){
+        int contador=0;
+        while(contador<50){
+            contador=contador+1;
+            editAgua.setText(contador);
+        }
+    }*/
 
 }
